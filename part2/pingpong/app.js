223198d440fec91e502bf42dee2de44b2ae07003
Santeri Nurminen <nusanteri@gmail.com>
2021-04-13 21:26:29 +0300

    2.07

--- part2/pingpong/app.js
+++ part2/pingpong/app.js
@@ -1,19 +1,67 @@
 const express = require('express');
-const fs = require('fs');
 const app = express();
 const port = 3002;
+const { Pool } = require('pg');
 
+const pool = new Pool();
 var counter = 0;
 
 app.get('/', (req, res) => {
   counter++;
+  pool.query('UPDATE counter SET counter = $1 WHERE id = true', [counter], (err, res) => {
+    if (err) {
+      console.log(err);
+    }
+  });
+
   res.send(`<p>pong ${counter}</p>`);
 });
 
 app.get('/pongs', (req, res) => {
-  res.send({ pongs: `Ping / Pongs: ${counter}` });
+  pool.query('SELECT * FROM counter', (err, dbres) => {
+    if (err) {
+      console.log(err);
+    }
+    
+    // Not the right way to do it but w.e. should work bc only 1 row should exist
+    res.send({ pongs: `Ping / Pongs: ${dbres.rows[0].counter}` });
+  });
 });
 
 app.listen(port, () => {
   console.log(`Server started in port ${port}`);
+  // Create schema if doesn't exist
+  pool.query('CREATE SCHEMA IF NOT EXISTS pingpong', (err, res) => {
+    if (err) {
+      console.log(err);
+      throw err;
+    }
+  });
+
+  // Create table if doesn't exist, makeshift restriction to allow just 1 row
+  // https://stackoverflow.com/questions/25307244/how-to-allow-only-one-row-for-a-table
+  pool.query(`CREATE TABLE IF NOT EXISTS counter (
+    ID BOOL PRIMARY KEY DEFAULT TRUE,
+    counter INTEGER NOT NULL DEFAULT '0',
+    CONSTRAINT onerow_uni CHECK (ID)
+  )`, (err, res) => {
+    if (err) {
+      console.log(err);
+    }
+
+    pool.query(`INSERT INTO counter (counter)
+    VALUES (0)`, (err, res) => {
+      if (err) {
+        console.log(err);
+      }
+    });
+  });
+
+  pool.query('SELECT * FROM counter', (err, res) => {
+    if (err) {
+      console.log(err);
+    }
+
+    counter = res.rows[0].counter;
+  })
 });
